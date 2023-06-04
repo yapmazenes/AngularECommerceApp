5bac433ed55815d48516b045a6bb4c4c6eaba2d6
Enes Yapmaz <enes.yapmaz@seyirmobil.com>
2023-06-04 13:47:10 +0300

    added File Repositories and serviceRegistrations

---
+++ ECommerce/Infrastructure/ECommerce.Persistence/Repositories/File/FileReadRepository.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Application.RepositoryAbstractions.File;
+using ECommerce.Persistence.Contexts;
+
+namespace ECommerce.Persistence.Repositories
+{
+    public class FileReadRepository : ReadRepository<Domain.Entities.File>, IFileReadRepository
+    {
+        public FileReadRepository(ECommerceDbContext context) : base(context)
+        {
+        }
+    }
+}
