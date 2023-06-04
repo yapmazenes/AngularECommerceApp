1bd227a6998d851ded0f3fcce40d59f1b3da0ea6
Enes Yapmaz <enes.yapmaz@seyirmobil.com>
2023-06-04 15:21:30 +0300

    some bugs fixed

--- ECommerce/Core/ECommerce.Application/Features/Queries/ProductImageFile/GetProductImages/GetProductImagesQueryRequest.cs
+++ ECommerce/Core/ECommerce.Application/Features/Queries/ProductImageFile/GetProductImages/GetProductImagesQueryRequest.cs
@@ -2,7 +2,7 @@
 
 namespace ECommerce.Application.Features.Queries.ProductImageFile.GetProductImages
 {
-    public class GetProductImagesQueryRequest : IRequest<List<GetProductImagesQueryResponse>>
+    public class GetProductImagesQueryRequest : IRequest<IEnumerable<GetProductImagesQueryResponse>>
     {
         public string Id { get; set; }
     }
