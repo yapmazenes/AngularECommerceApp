38dccbccbf5c73b043e1e61cd53ad07613b8d1ca
Enes Yapmaz <enes.yapmaz@seyirmobil.com>
2023-06-10 18:14:19 +0300

    test

--- ECommerceClient/src/app/guards/common/auth.guard.ts
+++ ECommerceClient/src/app/guards/common/auth.guard.ts
@@ -1,27 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { JwtHelperService } from '@auth0/angular-jwt';
+import { _isAuthenticated } from 'src/app/services/common/auth.service';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
 
-  const jwtHelper = inject(JwtHelperService);
+  //const authService = inject(AuthService);
   const router = inject(Router);
   const toastrService = inject(CustomToastrService);
 
-  const token = localStorage.getItem("accessToken");
-
-  //const decodeToken = jwtHelper.decodeToken(token);
-  //const expirationDate = jwtHelper.getTokenExpirationDate(token);
-  let isTokenExpired: boolean;
-
-  try {
-    isTokenExpired = jwtHelper.isTokenExpired(token);
-  } catch (error) {
-    isTokenExpired = true;
-  }
-
-  if (!token || isTokenExpired) {
+  if (!_isAuthenticated) {
     router.navigate(["login"], { queryParams: { returnUrl: state.url } });
     toastrService.message("You have to login!", "Unauthorized Access", {
       messageType: ToastrMessageType.Warning,
