50546109abee4ef55c23d86a2dd95a13e0595a2c
Enes Yapmaz <enes.yapmaz@seyirmobil.com>
2023-06-10 14:31:39 +0300

    improved user.service

--- ECommerceClient/src/app/services/common/user.service.ts
+++ ECommerceClient/src/app/services/common/user.service.ts
@@ -3,13 +3,16 @@ import { HttpClientService } from './http-client.service';
 import { User } from 'src/app/entities/user';
 import { CreateUser } from 'src/app/contracts/users/create-user';
 import { lastValueFrom } from 'rxjs';
+import { Token } from 'src/app/contracts/login/token';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../ui/custom-toastr.service';
+import { LoginResponse } from 'src/app/contracts/login/login-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClientService: HttpClientService) { }
+  constructor(private httpClientService: HttpClientService, private toastrService: CustomToastrService) { }
 
   async create(user: User): Promise<CreateUser> {
     const observable = this.httpClientService.post<CreateUser | User>({
@@ -19,15 +22,26 @@ export class UserService {
     return await lastValueFrom(observable) as CreateUser;
   }
 
-  async login(userNameOrEmail: string, password: string, callBackFunction?: () => void) {
+  async login(userNameOrEmail: string, password: string, callBackFunction?: () => void): Promise<any> {
 
-    const observable = this.httpClientService.post({
+    const observable = this.httpClientService.post<any | LoginResponse>({
       controller: "authentication",
       action: "login"
 
     }, { userNameOrEmail: userNameOrEmail, password: password })
 
-    await lastValueFrom(observable);
+    const loginResponse = await lastValueFrom(observable) as LoginResponse;
+    if (loginResponse) {
+      localStorage.setItem("accessToken", loginResponse.token.accessToken);
+
+      this.toastrService.message("User login successfully.", "Login Successfully", {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight
+
+      });
+
+    }
+
     callBackFunction();
   }
 }
